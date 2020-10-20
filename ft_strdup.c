8a314aa42680d6af9508e768cb6be8ba21b59e95
Mauro Monereo <mauromonereo@MacBook-Pro-de-Mauro.local>
2020-10-20 17:24:39 +0200

    normi dup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,13 @@
 /*   By: mmonereo <mmonereo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 17:04:15 by mmonereo          #+#    #+#             */
-/*   Updated: 2020/10/20 17:21:33 by mmonereo         ###   ########.fr       */
+/*   Updated: 2020/10/20 17:24:11 by mmonereo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strcpy(const char* dest, const char* src)
+static char	*ft_strcpy(const char *dest, const char *src)
 {
 	char	*dst;
 	size_t	i;
@@ -30,7 +30,7 @@ static char	*ft_strcpy(const char* dest, const char* src)
 
 static char	*ft_strnew(size_t size)
 {
-	char *new;
+	char	*new;
 
 	if (!(new = malloc(sizeof(char) * size)))
 		return (NULL);
