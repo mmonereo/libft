1b56ee68a1c632ab2bc417f1ff14fbf663fa564e
Mauro Monereo <mauromonereo@MacBook-Pro-de-Mauro.local>
2020-10-30 15:55:41 +0100

    split cuarto

--- ft_split.c
+++ ft_split.c
@@ -5,96 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmonereo <mmonereo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/17 18:42:16 by mmonereo          #+#    #+#             */
-/*   Updated: 2020/10/27 16:31:44 by mmonereo         ###   ########.fr       */
+/*   Created: 2020/10/26 17:32:49 by mmonereo          #+#    #+#             */
+/*   Updated: 2020/10/30 15:28:50 by mmonereo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		**ft_freetab(char **tab)
+static int	ft_wordcount(char const *s, char c)
 {
-	size_t i;
+	int		i;
+	int		count;
+	int		stop;
 
 	i = 0;
-	while (tab[i])
+	count = 0;
+	stop = 0;
+	while (s[i] != '\0')
 	{
-		free(tab[i]);
+		if ((s[i] == c) && (stop == 1))
+			stop = 0;
+		if ((stop == 0) && (s[i] != c))
+		{
+			stop = 1;
+			count++;
+		}
 		i++;
 	}
-	free(tab);
-	return (NULL);
+	return (count);
 }
 
-static size_t	ft_wordc(char const *s, char c)
+static int	ft_wordlen(char const *s, char c)
 {
-	size_t wcount;
-	size_t i;
+	int		i;
+	int		len;
 
-	wcount = 0;
 	i = 0;
-	while (s[i] == c || s[i] != 0)
-	{
-		i++;
-		while (s[i] != c && s[i] != 0)
-		{
-			wcount++;
-			while (s[i] != 0 && s[i] != c)
-			{
-				++i;
-			}
-		}
-	}
-	return (wcount);
-}
-
-static size_t	ft_wlen(const char *s, char c, size_t *j)
-{
-	size_t wlen;
-
-	wlen = 0;
-	if (s[0] != c && *j == 0)
-	{
-		while (s[0 + wlen] != 0 && s[0 + wlen] != c)
-		{
-			++wlen;
-		}
-	}
-	while (s[(*j)] == c)
-	{
-		*j += 1;
-		while (s[(*j) + wlen] != 0 && s[(*j) + wlen] != c)
-		{
-			++wlen;
-		}
-	}
-	return (wlen);
-}
-
-char			**ft_split(char const *s, char c)
-{
-	char	**newarr;
-	size_t	j;
-	size_t	i;
-	size_t	len;
-	size_t	wcount;
-
 	len = 0;
-	i = 0;
-	j = 0;
-	wcount = ft_wordc(s, c);
-	if (!(newarr = (char **)malloc(sizeof(char *) * wcount + 1)))
-		return (NULL);
-	while (s[j] != 0 && wcount--)
+	while ((s[i] != c) && (s[i] != '\0'))
 	{
-		len = ft_wlen(s, c, &j);
-		if (!(newarr[i] = ft_substr(s, j, len)))
-		{
-			return (ft_freetab(newarr));
-		}
-		j = j + len;
+		i++;
+		len++;
+	}
+	return (len);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	int		i;
+	int		wordc;
+	char	**new;
+
+	i = 0;
+	if (s && *s)
+		wordc = ft_wordcount(s, c);
+	else
+		wordc = 0;
+	new = (char **)malloc((wordc + 1) * sizeof(char *));
+	if (new == NULL)
+		return (NULL);
+	while (wordc--)
+	{
+		while ((*s == c) && (*s != '\0'))
+			s++;
+		new[i] = ft_substr(s, 0, ft_wordlen(s, c));
+		if (new[i] == NULL)
+			return (NULL);
+		s = s + ft_wordlen(s, c);
 		i++;
 	}
-	newarr[i] = 0;
-	return (&newarr[0]);
+	new[i] = NULL;
+	return (new);
 }
